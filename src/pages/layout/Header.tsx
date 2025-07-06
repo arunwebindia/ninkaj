00103c416db6827d50fff4ae2d9989a901fe7888
arun <hgsarun@gmail.com>
2025-07-06 14:24:00 +0530

    sidebar close button

--- src/pages/layout/Header.tsx
+++ src/pages/layout/Header.tsx
@@ -1,18 +1,25 @@
 // components/Header.tsx
+import { useRef, useState } from 'react';
 import { Navbar, Nav, Container, Offcanvas, Button } from 'react-bootstrap';
 import { Link, NavLink } from 'react-router-dom';
 
 export default function Header() {
+   const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
   return (
     <>
       <Navbar key="md" expand="md" bg="light" className="shadow-sm sticky-top">
         <Container>
-          <Navbar.Brand as={Link} to="/">MyShop</Navbar.Brand>
-          <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-md`} />
+          <Navbar.Brand as={Link} to="/">My Shop</Navbar.Brand>
+          <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-md`}  onClick={handleShow}/>
           <Navbar.Offcanvas
             id={`offcanvasNavbar-expand-lg`}
             aria-labelledby={`offcanvasNavbarLabel-expand-md`}
             placement="start"
+            show={show}
+          onHide={handleClose}
           >
             <Offcanvas.Header closeButton>
               <Offcanvas.Title id={`offcanvasNavbarLabel-expand-md`}>
@@ -22,14 +29,14 @@ export default function Header() {
             <Offcanvas.Body>
               <Nav className="justify-content-end flex-grow-1 pe-3">
                 <li>
-                <NavLink className={"nav-link"} to="/">Home</NavLink>
+                <NavLink className={"nav-link"} to="/" onClick={handleClose}>Home</NavLink>
                 </li>
                 <li>
-                <NavLink className={"nav-link"} to="/about">About</NavLink>
+                <NavLink className={"nav-link"} to="/about" onClick={handleClose}>About</NavLink>
 
                 </li>
                 <li>
-                <NavLink className={"nav-link"} to="/products">Products</NavLink>
+                <NavLink className={"nav-link"} to="/products" onClick={handleClose}>Products</NavLink>
 
                 </li>
                 <li>
